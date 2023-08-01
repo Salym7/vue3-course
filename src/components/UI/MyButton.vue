ad554482d6f199b30f1c9ecf2a8b4da514e81130
Oleksandr Solianyk <salym3k@gmail.com>
2023-08-01 22:20:00 +0300

    Add ui components

---
+++ src/components/UI/MyButton.vue
@@ -0,0 +1,24 @@
+<template>
+  <button class="btn"><slot></slot></button>
+</template>
+
+<script>
+export default {
+  name: "MyButton",
+}
+</script>
+
+<style scoped>
+.btn {
+  width: 100%;
+  border: 1px solid teal;
+  padding: 10px;
+  margin-top: 10px;
+  border-radius: 5px;
+  background: teal;
+}
+.btn-delete {
+  background: rgb(112, 2, 2);
+  color: #fff;
+}
+</style>
